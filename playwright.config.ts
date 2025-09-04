8409f417e2f9ce6c011d6e6b0c027311a3b36272
Bhagyashri Katare <bhagyashri.katare@waiin.com>
2025-09-04 21:41:51 +0530

    pipeline file added

--- playwright.config.ts
+++ playwright.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
     headless: true,
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
+    trace: 'on-first-retry',
     launchOptions: {
       slowMo: 500, // 500ms delay between actions
     },
